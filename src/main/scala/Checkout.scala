cf5b3819e3d3fbe6533dc27ea79ddf2441536968
Colm Cavanagh <colm.cavanagh@ontrackretail.co.uk>
2016-12-17 18:50:44 +0000

    Step 2 for checkout question

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,24 +1,44 @@
 
 
-trait ShoppingItems{
-  val name  : String
-  val price : Int
+trait ShoppingItems {
+  val name: String
+  val price: Int
 }
 
-case class Apples(name: String = "APPLES", price: Int = 60) extends ShoppingItems
+case class Apples(name: String = "APPLE", price: Int = 60) extends ShoppingItems
 
-case class Oranges(name: String = "ORANGES", price: Int = 25) extends ShoppingItems
+case class Oranges(name: String = "ORANGE", price: Int = 25) extends ShoppingItems
+
+case class DiscountRule(nameOfItem: String, amountOfItemForDiscount: Int, discountedPrice: Int)
 
 class Checkout {
 
-  def calculateTotalPrice(shoppingItems: List[String]): Int = {
-     shoppingItems.flatMap(matchItems).foldLeft(0)((totalPrice, item) => totalPrice +  item.price)
+  def calculateTotalPrice(shoppingItems: List[String], discounts: Option[List[DiscountRule]] = None): Int = {
+    val shoppingItemsAsOurData = shoppingItems.flatMap(matchItems)
+    val normalPrice = shoppingItemsAsOurData.foldLeft(0)((totalPrice, item) => totalPrice + item.price)
+    discounts match {
+      case Some(discount) => normalPrice - applyDiscounts(shoppingItemsAsOurData, discount)
+      case None => normalPrice
+    }
   }
- private def matchItems (itemName : String) : Option[ShoppingItems] = {
-       itemName.toUpperCase.trim match {
+
+  private def applyDiscounts(shoppingItems: List[ShoppingItems], discountRules: List[DiscountRule]): Int = {
+    val mapOfShoppingItems = shoppingItems.groupBy(_.name)
+    discountRules.map(dr => calculateAmountToDiscount(mapOfShoppingItems(dr.nameOfItem), dr.amountOfItemForDiscount, dr.discountedPrice)).sum
+  }
+
+  def calculateAmountToDiscount(listOfOneItem : List[ShoppingItems], amountNeededForDiscount: Int, setPrice: Int): Int = {
+    val numberOfDiscounts = listOfOneItem.size / amountNeededForDiscount
+    val normalCost = (listOfOneItem.headOption.map(_.price).getOrElse(0) * amountNeededForDiscount) * numberOfDiscounts
+    val totalDiscount = numberOfDiscounts * setPrice
+    normalCost - totalDiscount
+  }
+
+  private def matchItems(itemName: String): Option[ShoppingItems] = {
+    itemName.toUpperCase.trim match {
       case "APPLE" => Some(Apples.apply())
-      case "ORANGE" =>  Some(Oranges.apply())
-      case _ => None  //I am not going to worry about logging or exceptions fot this excerzixe ;)
+      case "ORANGE" => Some(Oranges.apply())
+      case _ => None //I am not going to worry about logging or exceptions fot this excerzixe ;)
     }
   }
 
--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -15,4 +15,17 @@ class CheckoutSpec extends FlatSpec {
     val listWithVoidItem = exampleInput ++ List("Bnanna")
     assert(checkoutService.calculateTotalPrice(listWithVoidItem) == 205)
  }
+  it should "take into account  discounts" in {
+    val listOfApples = List("APPLE","APPLE")
+    val discounts1 = Some(List(DiscountRule("APPLE" , 2 , 60)))
+    assert(checkoutService.calculateTotalPrice(listOfApples , discounts1) == 60)
+    val listOfApples2 = List("APPLE","APPLE", "APPLE")
+    assert(checkoutService.calculateTotalPrice(listOfApples2 , discounts1) == 120)
+    val listOfOrange =  List("ORANGE","ORANGE","ORANGE","ORANGE","ORANGE","ORANGE","ORANGE","ORANGE","ORANGE")
+    val discounts2 = Some(List(DiscountRule("ORANGE" , 3 , 50)))
+    assert(checkoutService.calculateTotalPrice(listOfOrange , discounts2) == 150)
+    val discounts3 = Some(List(DiscountRule("ORANGE" , 3 , 50),DiscountRule("APPLE" , 2 , 60)))
+    assert(checkoutService.calculateTotalPrice(listOfOrange ++ listOfApples, discounts3) == 210)
+  }
+
 }
